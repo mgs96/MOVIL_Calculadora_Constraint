3ce7a99e5d521582183b3b582aee454381ac378b
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-02-20 17:12:22 -0500

    adds minimum button functionality

--- app/src/main/java/com/example/mauricio/calculadora_constraint/MainActivity.java
+++ app/src/main/java/com/example/mauricio/calculadora_constraint/MainActivity.java
@@ -2,12 +2,78 @@ package com.example.mauricio.calculadora_constraint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView salida;
+    int operandoA;
+    int operandoB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        salida = (TextView) findViewById(R.id.textView);
+
+    }
+
+    public void onClick7(View view) {
+        salida.setText(salida.getText().toString() + "7");
+    }
+
+    public void onClick8(View view) {
+        salida.setText(salida.getText().toString() + "8");
+    }
+
+    public void onClick9(View view) {
+        salida.setText(salida.getText().toString() + "9");
+    }
+
+    public void onClick4(View view) {
+        salida.setText(salida.getText().toString() + "4");
+    }
+
+    public void onClick5(View view) {
+        salida.setText(salida.getText().toString() + "5");
+    }
+
+    public void onClick6(View view) {
+        salida.setText(salida.getText().toString() + "6");
+    }
+
+    public void onClick1(View view) {
+        salida.setText(salida.getText().toString() + "1");
+    }
+
+    public void onClick2(View view) {
+        salida.setText(salida.getText().toString() + "2");
+    }
+
+    public void onClick3(View view) {
+        salida.setText(salida.getText().toString() + "3");
+    }
+
+    public void onClickPLUS(View view) {
+        salida.setText(salida.getText().toString() + "+");
+    }
+
+    public void onClick0(View view) {
+        salida.setText(salida.getText().toString() + "0");
+    }
+
+    public void onClickEQUALS(View view) {
+    }
+
+    public void onClickDIV(View view) {
+        salida.setText(salida.getText().toString() + "/");
+    }
+
+    public void onClickClear(View view) {
+        salida.setText("");
     }
 }
